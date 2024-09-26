9ea7626342407c9adb5397759a697c05062dccc9
Ano2225 <ouatt0767@gmail.com>
2024-09-26 17:29:28 +0000

    Update Footer

--- my-app/src/app/sections/Footer.tsx
+++ my-app/src/app/sections/Footer.tsx
@@ -1,30 +1,112 @@
-import React from 'react'
-import { FaWhatsapp, FaFacebook, FaInstagram } from 'react-icons/fa';
+'use client'
 
+import React from 'react';
+import { FaWhatsapp, FaFacebook, FaInstagram } from 'react-icons/fa';
+import { useMediaQuery } from 'react-responsive';
+import { useEffect, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Footer = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+  const socials = [
+    {
+      name: 'Instagram',
+      image: '/insta.png',
+    },
+    {
+      name: 'Facebook',
+      image: '/facebook.png',
+    },
+    {
+      name: 'WhatsApp',
+      image: '/wha.png',
+    },
+  ];
+
+  const [hoveredSocial, setHoveredSocial] = useState<string | null>(null);
+  const [rotation, setRotation] = useState<number>(0);
+  const [cliked, setCliked] = useState<boolean>(false);
+  const animation = {
+    scale: cliked ? [1, 1.3, 1] : 1,
+    transition: { duration: 0.3 },
+  };
+
+  useEffect(() => {
+    const handleClick = () => {
+      setCliked(true);
+      setTimeout(() => {
+        setCliked(false);
+      }, 200);
+    };
+    window.addEventListener('click', handleClick);
+    return () => window.removeEventListener('click', handleClick);
+  }, [cliked]);
+
   return (
-   <footer className='py-5 bg-black text-white/60 border-t border-white/20 '>
+    <footer className='py-5 bg-black text-white/60 border-t border-white/20'>
       <div className='container'>
-        <div className='flex flex-col gap-4 sm:flex-row sm:justify-between'>
+        <div className='flex flex-col items-center gap-4 sm:flex-row sm:justify-between'>
           <div className='text-center'>
-            ©2024 WiniChange . All rights reserved
+            ©2024 WiniChange. All rights reserved
           </div>
           <ul className='flex justify-center gap-2.5'>
-           <li>
-            <FaWhatsapp size="1.5em"/>
-           </li>
-           <li>
-            <FaFacebook size="1.5em"/>
-           </li>
-           <li>
-            <FaInstagram size="1.5em"/>
-           </li>
-        </ul>
-       </div> 
+            {isMobile ? (
+              <div className="flex gap-2.5">
+                <FaInstagram size="1.5em" />
+                <FaFacebook size="1.5em" />
+                <FaWhatsapp size="1.5em" />
+              </div>
+            ) : (
+              socials.map((social, index) => (
+                <li
+                  className={`relative cursor-pointer px-5 py-2 transition-opacity duration-200 ${
+                    hoveredSocial && hoveredSocial !== social.name
+                      ? 'opacity-50'
+                      : 'opacity-100'
+                  }`}
+                  key={index}
+                  onMouseEnter={() => {
+                    setHoveredSocial(social.name);
+                    setRotation(Math.random() * 20 - 10);
+                  }}
+                  onMouseLeave={() => setHoveredSocial(null)}
+                  onClick={() => {
+                    setCliked(true);
+                  }}
+                >
+                  <span className="block text-lg">{social.name}</span>
+                  <AnimatePresence>
+                    {hoveredSocial === social.name && (
+                      <motion.div
+                        className="absolute bottom-0 left-0 right-0 flex h-full w-full items-center justify-center"
+                        animate={animation}
+                      >
+                        <motion.img
+                          key={social.name}
+                          src={social.image}
+                          alt={social.name}
+                          className="size-16"
+                          initial={{
+                            y: -40,
+                            rotate: rotation,
+                            opacity: 0,
+                            filter: 'blur(2px)',
+                          }}
+                          animate={{ y: -50, opacity: 1, filter: 'blur(0px)' }}
+                          exit={{ y: -40, opacity: 0, filter: 'blur(2px)' }}
+                          transition={{ duration: 0.2 }}
+                        />
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </li>
+              ))
+            )}
+          </ul>
+        </div>
       </div>
-   </footer>
-  )
-}
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
